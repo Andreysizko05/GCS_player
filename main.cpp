b1439fabc453baa33e7f9b85bc32bfb5b0ccd322
asyz <andreysizko05@gmail.com>
2026-05-02 23:21:24 +0300

    Changed proj_name

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "PID_GUI_" + QLocale(locale).name();
+        const QString baseName = "GCS_player_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
